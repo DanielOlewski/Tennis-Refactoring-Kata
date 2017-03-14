4efa46a0eb777a17ca65fd8bb575b9ec8263a5cc
Daniel Olewski <daniel.olewski@microsoft.com>
2017-03-13 21:46:44 -0700

    moved RawScore to separate file

---
+++ Tennis/RawScore.cs
@@ -0,0 +1,28 @@
+﻿namespace Tennis
+{
+	public class RawScore
+	{
+		public RawScore(uint p1score, uint p2score)
+		{
+			Player1Score = p1score;
+			Player2Score = p2score;
+		}
+
+		public RawScore() : this (0, 0)
+		{
+		}
+
+		public void Player1Scores()
+		{
+			Player1Score++;
+		}
+
+		public void Player2Scores()
+		{
+			Player2Score++;
+		}
+
+		public uint Player1Score { get; private set;  }
+		public uint Player2Score { get; private set; }
+	}
+}
--- Tennis/Tests/RawScoreCounting.cs
+++ Tennis/Tests/RawScoreCounting.cs
@@ -54,30 +54,4 @@ namespace Tennis.Tests
 			counter.Player2Score.ShouldEqual<uint>(5);
 		}
 	}
-
-	public class RawScore
-	{
-		public RawScore(uint p1score, uint p2score)
-		{
-			Player1Score = p1score;
-			Player2Score = p2score;
-		}
-
-		public RawScore() : this (0, 0)
-		{
-		}
-
-		public void Player1Scores()
-		{
-			Player1Score++;
-		}
-
-		public void Player2Scores()
-		{
-			Player2Score++;
-		}
-
-		public uint Player1Score { get; private set;  }
-		public uint Player2Score { get; private set; }
-	}
 }
